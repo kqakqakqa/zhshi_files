7733343ca8f46e78d9b8ce9b4a4d89afb20eedd0
kqakqakqa <58761476+kqakqakqa@users.noreply.github.com>
2025-07-21 12:40:04 +0800

    update: txt阅读器 功能完善

--- entry/src/main/js/default/app.js
+++ entry/src/main/js/default/app.js
@@ -1,10 +1,10 @@
 import app from '@system.app';
 
 export default {
-    onCreate() {
-        console.info(`${app.getInfo().appName} v${app.getInfo().versionName} onCreate`);
-    },
-    onDestroy() {
-        console.info(`${app.getInfo().appName} v${app.getInfo().versionName} onDestroy`);
-    }
+  onCreate() {
+    console.info(`${app.getInfo().appName} v${app.getInfo().versionName} onCreate`);
+  },
+  onDestroy() {
+    console.info(`${app.getInfo().appName} v${app.getInfo().versionName} onDestroy`);
+  }
 };
--- entry/src/main/js/default/pages/index/index.js
+++ entry/src/main/js/default/pages/index/index.js
@@ -1,27 +1,60 @@
 import file from '@system.file';
 import app from '@system.app';
 import router from '@system.router';
+import battery from '@system.battery';
+import brightness from '@system.brightness';
 
 export default {
   data: {
-    paths: ["//app"],
+    paths: ["/app/user"],
     files: [],
-    failData: "",
+    fileType: "",
     fileContent: "",
     showLeftSideSwipe: false,
     showBottomSideSwipe:false,
     leftSideSwipeTimeout:null,
     bottomSideSwipeTimeout: null,
+    deviceStatus: "",
+    openPosition: 0,
+    pageSize: 210,
+    showTitle: true,
   },
   onInit() {
     this.openPath();
+    brightness.setKeepScreenOn({
+      keepScreenOn: true
+    });
+    setInterval(()=>{
+      battery.getStatus({
+        success: data=>{
+          const batteryStatus = `${data.charging ? "充电中" : `电量${data.level*100}%`}`;
+          var date = new Date();
+          var hour = date.getHours();
+          var minute = date.getMinutes();
+          var time = `${hour < 10 ? "0" : ""}${hour}:${minute < 10 ? "0" : ""}${minute}`;
+          this.deviceStatus = `${batteryStatus}  ${time}`;
+        }
+      })
+    },1000);
   },
   onGoParentClick(){
     const currentPath = this.paths[this.paths.length - 1];
-    if(this.paths.length === 1 || currentPath.slice(-4) === "\\\\.."){
-      this.paths.push(currentPath + "\\\\..");
+
+    let len = currentPath.length;
+    while (len > 1 && currentPath.charAt(len - 1) === '/') {
+      len--;
+    }
+    let lastSlash = -1;
+    for (let i = len - 1; i >= 0; i--) {
+      if (currentPath.charAt(i) === '/') {
+        lastSlash = i;
+        break;
+      }
+    }
+    if (lastSlash <= 0) {
+      this.paths.push("/");
     } else {
-      return this.onGoBackClick();
+      this.paths.push(currentPath.substring(0, lastSlash));
     }
     this.openPath();
   },
@@ -33,45 +66,100 @@ export default {
   },
   onGoClick(item) {
     this.paths.push(this.paths[this.paths.length - 1] + "/" + item.uri);
+    this.openPosition = 0;
     this.openPath();
   },
+  onPrevPageClick(){
+    this.openPosition -= this.pageSize;
+    this.openPath();
+  },
+  onNextPageClick(){
+    this.openPosition += this.pageSize;
+    this.openPath();
+  },
+  onPageSwipe(data) {
+    switch (data.direction) {
+      case "up":
+        this.showTitle = false;
+        break;
+      case "down":
+        this.showTitle = true;
+        break;
+      default:
+        break;
+    }
+  },
   openPath(){
-    const currentPath = `internal:${this.paths[this.paths.length - 1]}`;
+    const currentPath = `internal://app\\\\..\\\\..\\\\..\\\\..${this.paths[this.paths.length - 1]}`;
     file.get({
       uri: currentPath,
       success: d => {
-        if (d.type === "dir") {
-          file.list({
-            uri: currentPath,
-            success: d => {
-              this.clearData();
-              this.files = d.fileList;
-            },
-            fail: this.showFailData
-          });
-        } else if (d.type === "file") {
-          file.readText({
-            uri: currentPath,
-            success: d => {
-              this.clearData();
-              this.fileContent = d.text;
-            },
-            fail: this.showFailData
-          })
+        switch (d.type) {
+          case "dir": {
+              file.list({
+                uri: currentPath,
+                success: d => {
+                  this.clearData();
+                  this.fileType = "folder";
+                  this.files = d.fileList;
+                },
+                fail: this.showFailData
+              });
+              break;
+            }
+          case "file": {
+            const fileExt = d.uri.split(".").slice(-2).join(".");
+            switch (fileExt) {
+              case "png.mp3": {
+                file.copy({
+                  srcUri: currentPath,
+                  dstUri: "internal://app/image.png",
+                  success: ()=>{
+                    this.clearData();
+                    this.fileType = "image";
+                  },
+                  fail: this.showFailData
+                })
+                break;
+              }
+              default: {
+                if (this.openPosition < 0) this.openPosition = 0;
+                else if (this.openPosition > d.length) this.openPosition = d.length;
+                file.readText({
+                  uri: currentPath,
+                  position: this.openPosition,
+                  length: this.pageSize,
+                  success: d => {
+                    this.clearData();
+                    this.fileType = "text";
+                    this.fileContent = d.text;
+                  },
+                  fail: this.showFailData
+                });
+                break;
+              }
+            }
+            break;
+          }
+          default:
+            break;
         }
       },
       fail: this.showFailData
     });
   },
   clearData(){
+    this.fileType = "";
     this.files = [];
-    this.failData = "";
     this.fileContent = "";
   },
   showFailData(data, code){
-    this.files = [];
-    this.failData = `${code} ${data}`;
-    this.fileContent = "";
+    this.clearData();
+    this.fileType = "fail";
+    let failData = `${code} ${data}`;
+    if(failData === "300 I/O error") failData += "\n(可能是空文件夹)";
+    this.fileContent = failData;
+    this.showTitle = true;
   },
   getListItemBgColor(i){
     const bgc = i.type === 'dir' ? '#ffa' : ( i.type === 'file' ? '#aaf' : '#fff');
